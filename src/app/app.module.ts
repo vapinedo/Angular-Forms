7710377eca07ff9bb08e351e57c79ce440f9300d
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-06-29 20:44:47 -0500

    [add] Binding Data with ngForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingComponents, AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
