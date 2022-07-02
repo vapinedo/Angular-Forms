880ea2d01635257bb6f08f222fe10adc401fcc63
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-07-02 13:17:47 -0500

    [add] reactive-form :: Adding form HTML

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
 import { TemplateFormComponent } from './components/template-form/template-form.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/template', pathMatch: 'full' },
   { path: 'template', component: TemplateFormComponent },
+  { path: 'reactive', component: ReactiveFormComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingComponents, AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     AppRoutingComponents,
+    ReactiveFormComponent,
   ],
   imports: [
     BrowserModule,
